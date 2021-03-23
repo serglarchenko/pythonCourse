6637c0c7ce04670ad32ee076a648a6b26c054bfa
Serhii_Larchenko <serhii_larchenko@epam.com>
2021-03-23 15:13:45 +0200

    hw_4 business model

--- lesson4/resources/business.py
+++ lesson4/resources/business.py
@@ -1,7 +1,7 @@
 import abc
 
-
 # Implement all methods where `NotImplementedError` is raised
+from abc import ABC
 
 
 class Company(object):
@@ -16,18 +16,22 @@ class Company(object):
     def add_employee(self, employee):
         # make sure employee is an instance of Engineer or Manager
         # make sure he is not employed already
-        raise NotImplementedError()
+        if (isinstance(employee, Engineer) or isinstance(employee, Manager)) and employee.is_employed is False:
+            return self.employees.append(employee)
 
     def dismiss_employee(self, employee):
         """
         Dismisses an employee. Employee must be a company member.
         Company should notify employee that he/she was dismissed
         """
-        raise NotImplementedError()
+        if employee.is_employed and employee.company.name == self.name:
+            print("You are fired")
+            self.employees.remove(employee)
+            employee.company = None
 
     def notify_im_leaving(self, employee):
         """ En employee should call this method when leaving a company """
-        raise NotImplementedError()
+        print(f"I am {employee.name} is going to leave {self.name}")
 
     def do_tasks(self, employee):
         """
@@ -38,7 +42,10 @@ class Company(object):
         """
         # make sure engineer is employed to this company
         # check employee is Engineer
-        raise NotImplementedError()
+        if employee.is_employed and isinstance(employee, Engineer):
+            self.__money = self.__money - 10
+            print('Task done')
+            return employee.put_money_into_my_wallet(10)
 
     def write_reports(self, employee):
         """
@@ -49,24 +56,33 @@ class Company(object):
         """
         # make sure manager is employed to this company
         # check employee is Manager
-        raise NotImplementedError()
+        if employee.is_employed and isinstance(employee, Manager):
+            self.__money = self.__money - 12
+            print('Report done!')
+            return employee.put_money_into_my_wallet(12)
 
     def make_a_party(self):
         """ Party time! All employees get 5 money """
         # make sure a company is not a bankrupt before and after the party
         # call employee.bonus_to_salary()
-        raise NotImplementedError()
+        if self.is_bankrupt is not True:
+            for employee in self.employees:
+                employee.bonus_to_salary(self)
+        else:
+            print('Company is bankrupt')
 
     def show_money(self):
         """ Displays amount of money that company has """
-        raise NotImplementedError()
+        return print(self.__money)
 
     def go_bankrupt(self):
         """
         Declare bankruptcy. Company money are drop to 0.
         All employees become unemployed.
         """
-        raise NotImplementedError()
+        self.__money = 0
+        for employee in self.employees:
+            self.dismiss_employee(employee)
 
     @property
     def is_bankrupt(self):
@@ -100,15 +116,23 @@ class Employee(Person):
 
     def join_company(self, company):
         # make sure that this person is not employed already
-        raise NotImplementedError()
+        if self.is_employed is False:
+            company.add_employee(self)
+            self.company = company
+            print(f"You already work on {company}")
+        else:
+            print("You can`t join to another company ")
 
     def become_unemployed(self):
         """ Leave current company """
-        raise NotImplementedError()
+        if self.is_employed is True:
+            self.notify_dismissed()
+            self.company.dismiss_employee(self)
+            self.company = None
 
     def notify_dismissed(self):
         """ Company should call this method when dismissing an employee """
-        raise NotImplementedError()
+        print(f'You are fired from our {self.company}')
 
     def bonus_to_salary(self, company, reward=5):
         """
@@ -116,27 +140,34 @@ class Employee(Person):
         """
         # make sure person is employed to same company
         # money + 5
-        raise NotImplementedError()
+        if company.name == self.company.name:
+            self.put_money_into_my_wallet(reward)
+            self.__money = self.__money - reward
 
     @property
     def is_employed(self):
         """ returns True or False """
         return self.company is not None
 
+    # @is_employed.setter
+    # def is_employed(self, value):
+    #     self.company = value
+
     def put_money_into_my_wallet(self, amount):
         """ Adds the indicated amount of money to persons budget """
         # Engineer and Manager will have to use this method to store their
         # salary, because __money is a private attribute
-        raise NotImplementedError()
+        self.__money = self.__money + amount
+        return self.__money
 
     def show_money(self):
         """ Shows how much money person has earned """
-        raise NotImplementedError()
+        return print(self.__money)
 
     @abc.abstractmethod
     def do_work(self):
         """ This method requires re-implementation """
-        raise NotImplemented('This method requires re-implementation')
+        self.company.do_tasks()
 
     def __repr__(self):
         if self.is_employed:
@@ -145,11 +176,18 @@ class Employee(Person):
 
 
 class Engineer(Employee):
-    pass  # implement me
+
+    def do_work(self):
+        self.company.do_tasks(self)
+
+
+# implement me
 
 
 class Manager(Employee):
-    pass  # implement me
+
+    def do_work(self):
+        self.company.write_reports(self)
 
 
 def check_yourself():
@@ -211,4 +249,3 @@ def check_yourself():
 
 if __name__ == '__main__':
     check_yourself()
-
